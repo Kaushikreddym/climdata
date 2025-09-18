db7d073f9d3e40473df46fc95e0a6ee26ff428db
Kaushikreddym <kaushikreddym007@gmail.com>
2025-09-18 16:57:23 +0200

    pypi failing

--- tests/test_climdata.py
+++ tests/test_climdata.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
 
-"""Tests for `climdata` package."""
-
-
 import unittest
-
-from climdata import climdata
-
+import climdata
 
 class TestClimdata(unittest.TestCase):
-    """Tests for `climdata` package."""
+    def test_version(self):
+        self.assertIsNotNone(climdata.__version__)
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
