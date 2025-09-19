20537f8d956b0ee4fa09f5b81e698117275dea7d
Kaushikreddym <kaushikreddym007@gmail.com>
2025-09-19 12:53:31 +0200

    Bump version: 0.0.4 → 0.0.5

--- climdata/__init__.py
+++ climdata/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Kaushik Muduchuru"""
 __email__ = "kaushik.reddy.m@gmail.com"
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from .utils.utils_download import * # etc.
 from .utils.config import load_config
