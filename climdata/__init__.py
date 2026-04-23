239a18b9d788f5801e532609a56ef93835c91cbc
Kaushikreddym <kaushikreddym007@gmail.com>
2026-04-23 20:46:43 +0200

    Bump version: 0.5.1 → 0.5.2

--- climdata/__init__.py
+++ climdata/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Kaushik Muduchuru"""
 __email__ = "kaushik.reddy.m@gmail.com"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 from .utils.config import load_config
 from .datasets.DWD import DWDmirror as DWD
